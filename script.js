d097026466c124232cf7cd8fe8930a910c273673
samolazov <samolazov@users.noreply.github.com>
2016-12-05 15:38:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function getDPI() { //check dpi
     if (!dpi) {
         dpi = document.createElement("div");
         dpi.id = "dpi";
+        dpi.style.position = "absolute";
+        dpi.style.height = dpi.style.width = "1in";
+        dpi.style.top = dpi.style.left = "-100%";
         document.body.appendChild(dpi);
     }
     return !!(window.innerWidth > 1200 && dpi.offsetHeight < 200);
